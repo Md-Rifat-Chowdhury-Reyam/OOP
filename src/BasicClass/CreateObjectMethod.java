6fcb35a3716554eb9b9826693dd2f5e7ff025b69
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-26 01:20:48 +0600

    newinstance()

---
+++ src/BasicClass/CreateObjectMethod.java
@@ -0,0 +1,23 @@
+package BasicClass;
+
+import java.lang.reflect.*;
+
+public class CreateObjectMethod
+{
+    String search = "Create";
+    public static void main(String[] args)
+    {
+        try {
+            Class<?> c = Class.forName("BasicClass.CreateObjectMethod");
+            CreateObjectMethod cl =  (CreateObjectMethod) c.getDeclaredConstructor().newInstance();
+
+            System.out.println(cl.search);
+        }
+
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+               NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
