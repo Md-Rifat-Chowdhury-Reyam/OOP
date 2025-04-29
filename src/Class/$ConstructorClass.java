36f0e3ad4f205f99e2fbf364741063740c5a44d9
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-29 20:43:50 +0600

    constructor

---
+++ src/Class/$ConstructorClass.java
@@ -0,0 +1,17 @@
+package Class;
+
+public class $ConstructorClass {
+
+    $ConstructorClass()
+    {
+        System.out.println( "Constructor called");
+    }
+
+
+    public static void main(String[] args) {
+
+        $ConstructorClass cc = new $ConstructorClass();
+        // no need to call constructor method;
+
+    }
+}
