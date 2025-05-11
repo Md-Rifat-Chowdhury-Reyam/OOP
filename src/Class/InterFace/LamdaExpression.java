62806898af427fc4abb6fc68609e6b6c63665a2e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-11 19:34:55 +0600

    lamda expression

--- src/Class/InterFace/LamdaExpression.java
+++ src/Class/InterFace/LamdaExpression.java
@@ -4,23 +4,35 @@ public class LamdaExpression {
 
     public static void main(String[] args) {
 
-        FunctionalInterface fi = (int a) ->
-        {
-            System.out.println(2 * a);
-        };
+        // Lambda expression with zero parameters
+        Addiotionalable ad = () -> System.out.println("this is zero parameter lambda expression ");
+        ad.Method1();
 
-        fi.abstractFun(2);
+        //Lambda with a Single Parameter:
+        Additionable2 ad2 = ((i) -> System.out.println("single parameter " + i));
+        ad2.Method2(2);
 
+        //Lambda with a multiple Parameter:
+        Addiotionable3 ad3 = ((a, b) -> System.out.println("Multiple parameter : " + a + " , "  + b));
+        ad3.Method3(2,3);
     }
 }
 
-interface FunctionalInterface
+interface Addiotionalable  // functional interface (An interface with
+                            // single abstract method)
 {
-    void abstractFun (int a);
+    abstract void Method1(); //zero parameter
 
-    default void func ()
-    {
-        System.out.println("hello  functional interface");
-    }
+}
+
+interface Additionable2
+{
+    abstract void Method2(int i); //single parameter
+}
+
+interface  Addiotionable3
+{
+    abstract void Method3(int a , int b); //multiple parameter
+
 }
 
