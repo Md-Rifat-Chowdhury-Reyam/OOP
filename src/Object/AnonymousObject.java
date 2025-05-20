bda54f8e62fd15a4b9b6ce01902c842c37adff64
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-20 11:15:30 +0600

    anonymous object

---
+++ src/Object/AnonymousObject.java
@@ -0,0 +1,28 @@
+//package Object;
+//import javafx.application.Application;
+//
+//public class AnonymousObject extends Application {
+//    public static void main(String[] args) {
+//
+//        launch(args);
+//
+//
+//    }
+//    @Override
+//
+//    public void start(Stage stage) {
+//
+//        Button button = new Button("Click Me");
+//
+//        button.setOnAction(event -> System.out.println("Button clicked!")); // Anonymous object via lambda
+//
+//        StackPane root = new StackPane(button);
+//
+//        stage.setScene(new Scene(root, 300, 200));
+//
+//        stage.show();
+//
+//    }
+//
+//
+//}
