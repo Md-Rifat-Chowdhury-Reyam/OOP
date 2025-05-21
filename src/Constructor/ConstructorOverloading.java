6adc8762169f3a26c94a0918e056fc7808668fb2
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-21 08:39:41 +0600

    constructor overloading

--- src/Constructor/ConstructorOverloading.java
+++ src/Constructor/ConstructorOverloading.java
@@ -2,5 +2,23 @@ package Constructor;
 
 public class ConstructorOverloading {
 
+    ConstructorOverloading()
+    {
+        System.out.println("I am default");
+
+    }
+    ConstructorOverloading(String name, int i)
+    {
+        System.out.println("name : " + name + " number : " + i);
+    }
+    ConstructorOverloading(float a)
+    {
+        System.out.println("float number : " + a);
+    }
+
+    public static void main(String[] args) {
+        ConstructorOverloading co = new ConstructorOverloading(2);
+    }
+
     
 }
