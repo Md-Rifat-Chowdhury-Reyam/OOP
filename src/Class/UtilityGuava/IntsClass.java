ecdc8c27d40f16663fabe9c5bcd13a97330a076a
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-21 15:05:53 +0600

    Ints utility class for guava libraires

---
+++ src/Class/UtilityGuava/IntsClass.java
@@ -0,0 +1,24 @@
+package Class.UtilityGuava;
+
+import com.google.common.primitives.Ints;
+import java.util.*;
+
+/**
+ * Guava is a set of core Java libraries from Google,
+ * offering a wide range of utilities that extend the capabilities of the standard Java Development Kit (JDK).
+ * Ints is a utility class for primitive type int.
+ * It provides Static utility methods pertaining to int primitives,
+ * that are not already found in either Integer or Arrays
+ */
+
+public class IntsClass {
+
+    public static void main(String[] args) {
+        int[] arr1 = { 5, 10, 15 };
+        int[] arr2 = { 20, 25 };
+
+        int[] arrConCat = Ints.concat(arr1, arr2);
+
+        System.out.println(Arrays.toString(arrConCat));
+    }
+}
