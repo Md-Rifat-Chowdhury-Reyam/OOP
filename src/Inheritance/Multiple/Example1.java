e7626e792059891eb34a1328904c4714fb9d68b8
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-23 17:22:59 +0600

    Multiple inheritance

--- src/Inheritance/Multiple/Example1.java
+++ src/Inheritance/Multiple/Example1.java
@@ -1,11 +1,14 @@
 package Inheritance.Multiple;
 
+//Note: Java doesn't support Multiple Inheritance
+
+
 public class Example1 {
     public static void main(String[] args) {
 
         DevOp d =  new DevOp();
         d.writeCode();
-        d.writeCode();
+        d.testCode();
     }
 }
 
@@ -17,17 +20,23 @@ interface Coder
 interface Tester
 {
     void testCode();
+
+    default void info() //default method
+    {
+        System.out.println("interface allow default");
+    }
 }
 
 class DevOp implements Coder, Tester
 {
     @Override
     public void writeCode() {
-        System.out.println("write code");
+        System.out.println("DevOps write code");
     }
 
     @Override
     public void testCode() {
         System.out.println("test code");
+        Tester.super.info();
     }
 }
