aae848527185604dd7b7ba012c65b845941c4989
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-27 00:24:48 +0600

    nested interface insite class

--- src/Interface/functional/predicate/PredicateSAMForFiltering.java
+++ src/Interface/functional/predicate/PredicateSAMForFiltering.java
@@ -1,6 +1,5 @@
-package Interface.functional;
+package Interface.functional.predicate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
---
+++ src/Interface/nested/NestedInterfaceInsideClass.java
@@ -0,0 +1,29 @@
+package Interface.nested;
+
+public class NestedInterfaceInsideClass {
+    public static void main(String[] args) {
+        Outer.InnerAble obj;
+        Child ch = new Child();
+        ch.print();
+
+        //or
+        obj = ch;
+        obj.print();
+
+    }
+}
+class Outer //parent class
+{
+    interface InnerAble //nested interface
+    {
+        //by default abstract method
+        void print();
+    }
+}
+class Child implements Outer.InnerAble //child class
+{
+    @Override
+    public void print() {
+        System.out.println("I override from nested interface");
+    }
+}
