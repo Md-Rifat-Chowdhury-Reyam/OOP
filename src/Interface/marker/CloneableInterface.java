267ebd3012d9fc5a8a6a4d5803df7e72ca5deb31
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-27 01:21:38 +0600

    marker interface Cloneable

---
+++ src/Interface/marker/CloneableInterface.java
@@ -0,0 +1,28 @@
+package Interface.marker;
+
+public class CloneableInterface {
+    public static void main(String[] args) throws CloneNotSupportedException{
+
+        ParentClass pc = new ParentClass(20);
+
+        //
+        ParentClass pc2 = (ParentClass) pc.clone();
+        System.out.println(pc2.i);
+
+    }
+}
+class ParentClass implements Cloneable
+{
+    int i;
+    ParentClass(int a)
+    {
+        this.i = a;
+
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+}
