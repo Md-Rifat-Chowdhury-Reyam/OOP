28982e68f9804103441fadcb17d8ae184db596fa
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-26 12:35:21 +0600

    functional interface with lamda expression

---
+++ src/LamdaExpression.java
@@ -0,0 +1,23 @@
+public class LamdaExpression {
+
+    public static void main(String[] args) {
+
+        // lambda expression to
+        // define the calculate method
+        LamdaAble l = (i) -> i+i;
+
+        // parameter passed and return type must be
+        // same as defined in the prototype
+        int a = 4;
+        int sum = l.calculte(a);
+        System.out.println(sum);
+
+
+    }
+}
+
+@FunctionalInterface
+interface LamdaAble
+{
+    int calculte(int i); //by default abstract method
+}
